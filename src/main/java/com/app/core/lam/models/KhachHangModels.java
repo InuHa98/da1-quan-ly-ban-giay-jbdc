ae025396f455e5b2184b01c22d1f6a7318a910c8
LAM\Admin <lamnd1501@gmail.com>
2024-08-04 20:23:56 +0700

    update code

--- src/main/java/com/app/core/lam/models/KhachHangModels.java
+++ src/main/java/com/app/core/lam/models/KhachHangModels.java
@@ -4,23 +4,13 @@
  */
 package com.app.core.lam.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import java.time.LocalDateTime;
+
 
 /**
  *
  * @author Admin
  */
-@Getter
-@Setter
-@Builder
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 
 public class KhachHangModels {
 
@@ -33,7 +23,83 @@ public class KhachHangModels {
     private boolean gioiTinh;
 
     private String diaChi;
+    
+    private LocalDateTime ngayTao;
 
     private boolean trangThaiXoa;
 
+    public KhachHangModels() {
+    }
+
+    public KhachHangModels(int idKH, String tenKH, String soDienThoai, boolean gioiTinh, String diaChi, boolean trangThaiXoa) {
+        this.idKH = idKH;
+        this.tenKH = tenKH;
+        this.soDienThoai = soDienThoai;
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        this.ngayTao = LocalDateTime.now();
+        this.trangThaiXoa = trangThaiXoa;
+    }
+
+    public int getIdKH() {
+        return idKH;
+    }
+
+    public void setIdKH(int idKH) {
+        this.idKH = idKH;
+    }
+
+    public String getTenKH() {
+        return tenKH;
+    }
+
+    public void setTenKH(String tenKH) {
+        this.tenKH = tenKH;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public boolean isGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public LocalDateTime getNgayTao() {
+        return ngayTao;
+    }
+
+    public void setNgayTao(LocalDateTime ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+
+    public boolean isTrangThaiXoa() {
+        return trangThaiXoa;
+    }
+
+    public void setTrangThaiXoa(boolean trangThaiXoa) {
+        this.trangThaiXoa = trangThaiXoa;
+    }
+
+    @Override
+    public String toString() {
+        return "KhachHangModels{" + "idKH=" + idKH + ", tenKH=" + tenKH + ", soDienThoai=" + soDienThoai + ", gioiTinh=" + gioiTinh + ", diaChi=" + diaChi + ", ngayTao=" + ngayTao + ", trangThaiXoa=" + trangThaiXoa + '}';
+    }
+    
 }
--- src/main/java/com/app/core/lam/repositories/KhachHangRepositories.java
+++ src/main/java/com/app/core/lam/repositories/KhachHangRepositories.java
@@ -11,12 +11,88 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import com.app.core.lam.models.KhachHangModels;
 import com.app.core.lam.models.LichSuModels;
+import java.time.LocalDateTime;
 
 /**
  *
  * @author Admin
  */
 public class KhachHangRepositories {
+
+    public ArrayList<LichSuModels> getLS() {
+        String sql = """
+                 SELECT   
+                    KhachHang.id,
+                    HoaDon.ma,  
+                    HoaDonChiTiet.gia_ban,  
+                    HoaDonChiTiet.so_luong,  
+                    KhachHang.ngay_tao,  
+                    KhachHang.trang_thai_xoa
+                 FROM   
+                     HoaDon JOIN HoaDonChiTiet ON HoaDon.id = HoaDonChiTiet.id_hoa_don  
+                 JOIN   
+                     KhachHang ON HoaDon.id_khach_hang = KhachHang.id;
+                        
+                 """;
+        ArrayList<LichSuModels> listRPLS = new ArrayList<>();
+        try (Connection con = DBConnect.getInstance().getConnect();
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();) {
+            while (rs.next()) {
+                LichSuModels ls = new LichSuModels();
+                ls.setIdKH(rs.getInt(1));
+                ls.setMaHD(rs.getString(2));
+                ls.setGiaBan(rs.getDouble(3));
+                ls.setSoLuong(rs.getInt(4));
+                ls.setNgayMua(rs.getObject(5, LocalDateTime.class));
+                ls.setTrangThaiXoa(rs.getBoolean(6));
+                listRPLS.add(ls);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Data Retrieved: " + listRPLS.size());
+        return listRPLS;
+    }
+
+    public boolean deleteLichSu(String maHD) {
+        String sqlDeleteHoaDon = "DELETE FROM HoaDon WHERE maHD = ?";
+
+        int checkHoaDon = 0;
+
+        try (Connection con = DBConnect.getInstance().getConnect();
+                PreparedStatement ps = con.prepareStatement(sqlDeleteHoaDon)) {
+
+            ps.setString(1, maHD);
+            checkHoaDon = ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return checkHoaDon > 0;
+    }
+
+    public boolean updateLichSu(LichSuModels ls) {
+        String sql = """
+                    UPDATE LichSu
+                    SET giaBan = ?, soLuong = ?, trangThaiXoa = ?
+                    WHERE maHD = ?
+                    """;
+        int check = 0;
+        try (Connection con = DBConnect.getInstance().getConnect();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setDouble(1, ls.getGiaBan());
+            ps.setInt(2, ls.getSoLuong());
+            ps.setBoolean(3, ls.isTrangThaiXoa());
+            ps.setString(4, ls.getMaHD());
+            check = ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return check > 0;
+    }
 
     public ArrayList<KhachHangModels> getKH() {
         String sql = """
@@ -25,6 +101,7 @@ public class KhachHangRepositories {
                            ,[sdt]
                            ,[gioi_tinh]
                            ,[dia_chi]
+                           ,[ngay_tao]
                            ,[trang_thai_xoa]
                        FROM [dbo].[KhachHang]
                      """;
@@ -39,10 +116,11 @@ public class KhachHangRepositories {
                 kh.setSoDienThoai(rs.getString(3));
                 kh.setGioiTinh(rs.getBoolean(4));
                 kh.setDiaChi(rs.getString(5));
-                kh.setTrangThaiXoa(rs.getBoolean(6));
+                kh.setNgayTao(rs.getObject(6, LocalDateTime.class));
+                kh.setTrangThaiXoa(rs.getBoolean(7));
                 listRPKH.add(kh);
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,9 +130,13 @@ public class KhachHangRepositories {
     public boolean addKhachHang(KhachHangModels kh) {
         String sql = """
                  INSERT INTO [dbo].[KhachHang]
-                    ([ho_ten], [sdt], [gioi_tinh], [dia_chi], [trang_thai_xoa])
-                 VALUES
-                    (?, ?, ?, ?, ?)
+                            ([ho_ten]
+                            ,[sdt]
+                            ,[gioi_tinh]
+                            ,[dia_chi]
+                            ,[trang_thai_xoa])
+                      VALUES
+                            (?,?,?,?,?)
                  """;
         int check = 0;
         try (Connection con = DBConnect.getInstance().getConnect();
@@ -76,20 +158,27 @@ public class KhachHangRepositories {
         String sql = """
                      UPDATE [dbo].[KhachHang]
                         SET [ho_ten] = ?
+                           ,[sdt] = ?
                            ,[gioi_tinh] = ?
                            ,[dia_chi] = ?
                            ,[trang_thai_xoa] = ?
-                      WHERE  sdt = ?
+                      WHERE  id = ?
                      """;
         int check = 0;
         try (Connection con = DBConnect.getInstance().getConnect();
                 PreparedStatement ps = con.prepareStatement(sql)) {
 
             ps.setString(1, kh.getTenKH());
-            ps.setBoolean(2, kh.isGioiTinh());
-            ps.setString(3, kh.getDiaChi());
-            ps.setBoolean(4, kh.isTrangThaiXoa());
-            ps.setString(5, kh.getSoDienThoai());
+            ps.setString(2, kh.getSoDienThoai());
+            ps.setBoolean(3, kh.isGioiTinh());
+            ps.setString(4, kh.getDiaChi());
+            ps.setBoolean(5, kh.isTrangThaiXoa());
+            if (kh != null) {
+                int id = kh.getIdKH();
+                ps.setInt(6, id);
+            } else {
+                System.out.println("KhachHangModels is null");
+            }
             check = ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
@@ -108,66 +197,6 @@ public class KhachHangRepositories {
 
             ps.setString(1, soDienThoai);
 
-            check = ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return check > 0;
-    }
-
-    public ArrayList<LichSuModels> getLS() {
-        String sql = """
-                     SELECT 
-                            [id]
-                           ,[ho_ten]
-                           ,[sdt]
-                           ,[gioi_tinh]
-                           ,[dia_chi]
-                           ,[trang_thai_xoa]
-                       FROM [dbo].[KhachHang]
-                     """;
-        ArrayList<LichSuModels> listRPLS = new ArrayList<>();
-        try (Connection con = DBConnect.getInstance().getConnect();
-                PreparedStatement ps = con.prepareStatement(sql);
-                ResultSet rs = ps.executeQuery();) {
-            while (rs.next()) {
-                LichSuModels ls = new LichSuModels();
-                ls.setTenKH(rs.getString(1));
-                ls.setTenKH(rs.getString(2));
-                ls.setSoDienThoai(rs.getString(3));
-                ls.setGioiTinh(rs.getBoolean(4));
-                ls.setDiaChi(rs.getString(5));
-                ls.setTrangThaiXoa(rs.getBoolean(6));
-                listRPLS.add(ls);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return listRPLS;
-    }
-    
-    public boolean deleteLichSu(String soDienThoai) {
-        return deleteKhachHang(soDienThoai);
-    }
-    
-    public boolean updateLichSu(LichSuModels ls) {
-        String sql = """
-                     UPDATE [dbo].[KhachHang]
-                        SET [ho_ten] = ?
-                           ,[gioi_tinh] = ?
-                           ,[dia_chi] = ?
-                           ,[trang_thai_xoa] = ?
-                      WHERE  sdt = ?
-                     """;
-        int check = 0;
-        try (Connection con = DBConnect.getInstance().getConnect();
-                PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setString(1, ls.getTenKH());
-            ps.setBoolean(2, ls.isGioiTinh());
-            ps.setString(3, ls.getDiaChi());
-            ps.setBoolean(4, ls.isTrangThaiXoa());
-            ps.setString(5, ls.getSoDienThoai());
             check = ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
